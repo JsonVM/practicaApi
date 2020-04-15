4e3a6c259507b597da6bb97d807266d8ef49cc5b
JsonVM <48698637+JsonVM@users.noreply.github.com>
2020-04-15 12:31:22 -0500

    arreglando error cors

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+//añadiendo express
 const express = require("express");
+const cors = require("cors");
 
+//inicializar la libreria
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Api de páginas favoritas");
   });
   
+//ruta con su propio endpoint
 const rutas_paginas = require('./routes/paginas')
 app.use(rutas_paginas);
 
